8e9f49ca6005fc17cdb5afebec4e6754ac26eb62
Boaz <boazbaum100@gmail.com>
2024-07-01 23:42:51 +0300

    Add files

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,9 +11,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor(private service:OrderDetailsService) { }
-foodData:any;
+  foodData:any;
   ngOnInit(): void {
-this.foodData=this.service.foodDetails;
+  this.foodData=this.service.foodDetails;
 
   }
 
