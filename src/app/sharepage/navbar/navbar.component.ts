ecb998c4b1e6f74052fc1128517b2430320bc125
Boaz <boazbaum100@gmail.com>
2024-08-11 17:01:06 +0300

    initial commit

--- src/app/sharepage/navbar/navbar.component.ts
+++ src/app/sharepage/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  closeNavbar() {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+      toggle: false
+    });
+    bsCollapse.hide();
   }
 
 }
